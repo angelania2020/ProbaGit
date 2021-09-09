4bcf39064b7e8d665b1c71c1fd78f49f8b263145
Angelina <angelania@ya.ru>
2021-09-09 18:57:04 +0300

    add third version

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -17,6 +17,7 @@ public class ProbaGit {
     public static void main(String[] args) {
         System.out.println("First version of the program");
         System.out.println("Second version of the program");
+        System.out.println("Third version of the program");
     }
     
 }
